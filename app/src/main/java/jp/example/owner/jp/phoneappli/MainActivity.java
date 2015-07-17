f6e735549869bb89cac92c4d7e842313355724d2
milkboy-music <perspirationanddestiny@gmail.com>
2015-07-17 19:22:06 +0900

    test2

--- app/src/main/java/jp/example/owner/jp/phoneappli/MainActivity.java
+++ app/src/main/java/jp/example/owner/jp/phoneappli/MainActivity.java
@@ -1,5 +1,7 @@
 package jp.example.owner.jp.phoneappli;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,9 +9,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,31 +25,10 @@ public class MainActivity extends ActionBarActivity {
 
         setSupportActionBar(toolbar);
 
+
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-//インテントの宣言
-        Intent i;
-        int id = item.getItemId();
-
-
-        switch (id)
-        {
-          case R.id.action_PhoneBook :
-              i=new Intent("android.intent.action.VIEW", Uri.parse("content://contacts/people/"));
-              startActivity(i);
-            break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/jp/example/owner/jp/phoneappli/MainActivityFragment.java
+++ app/src/main/java/jp/example/owner/jp/phoneappli/MainActivityFragment.java
@@ -1,14 +1,17 @@
 package jp.example.owner.jp.phoneappli;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -23,6 +26,9 @@ import java.net.URI;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    EditText editText;
+
+    private String[] number = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};//セットする値＋asterisk,sharp
 
     public MainActivityFragment() {
     }
@@ -30,7 +36,52 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //オプションメニューを有効にする
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_main_activity, container, false);
+    }
+
+    //optionmenu
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //インテントの宣言
+        Intent i;
+        int id = item.getItemId();
+
+
+        switch (id) {
+            case R.id.action_PhoneBook:
+                i = new Intent("android.intent.action.VIEW", Uri.parse("content://contacts/people/"));//電話帳を開く
+                startActivity(i);
+                break;
+            case R.id.action_ResisterBook:
+
+                i = new Intent(ContactsContract.Intents.Insert.ACTION);//電話帳に番号を登録する
+                i.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                i.putExtra(ContactsContract.Intents.Insert.PHONE, editText.getText().toString());
+                startActivity(i);
+                // Creates a new Intent to insert a contact
+                //Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+// Sets the MIME type to match the Contacts Provider
+                //intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                //intent.putExtra(ContactsContract.Intents.Insert.PHONETIC_NAME, editText.getText().toString());//電話帳に名前を登録する
+//startActivity(intent);
+                break;
+            case R.id.action_SearchOnWeb:
+                String url=String.format("%s",Uri.encode(editText.getText().toString()));
+                i = new Intent(Intent.ACTION_WEB_SEARCH,Uri.parse(url));
+                startActivity(i);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -38,7 +89,7 @@ public class MainActivityFragment extends Fragment {
         super.onStart();
         //1 count=文字数　2 editText=数字入力 3 デリートボタン
         final TextView textView_count = (TextView) getActivity().findViewById(R.id.textView);
-        final EditText editText = (EditText) getActivity().findViewById(R.id.et_phone);
+        editText = (EditText) getActivity().findViewById(R.id.et_phone);
         final ImageButton btn_delete = (ImageButton) getActivity().findViewById(R.id.btn_delete);
         View img_phone = getActivity().findViewById(R.id.img_phone);
         editText.addTextChangedListener(new TextWatcher() {
@@ -65,8 +116,14 @@ public class MainActivityFragment extends Fragment {
         img_phone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + editText.getText().toString()));
-                startActivity(i);
+                if (!editText.getText().toString().equals("")) {
+                    //sharp=Uri.encode("#");
+                    Uri numbers = Uri.parse(String.format("tel:%s", Uri.encode(editText.getText().toString())));//Uri.encodeで#の記号を変換!!
+                    Intent i = new Intent(Intent.ACTION_DIAL, numbers);
+                    startActivity(i);
+                    //# ♯ # ＃　♯
+                }
+
             }
         });
 
@@ -77,7 +134,7 @@ public class MainActivityFragment extends Fragment {
 
                 String str = editText.getText().toString();
                 if (str.length() > 0) {
-                    editText.setText(str.substring(0, str.length() - 1));
+                    editText.setText(str.substring(0, str.length() - 1));//(start,end)
                     editText.setSelection(str.length() - 1);
 
 
@@ -104,25 +161,82 @@ public class MainActivityFragment extends Fragment {
 
 
         });
-        //数字ボタンを押したらエディットテキストに数字が入る実装
+        //数字ボタンを押したらエディットテキストに数字が入る実装(0~9とアスタリスクとシャープ)
+        getActivity().findViewById(R.id.imv0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[0]);
+            }
+        });
+        getActivity().findViewById(R.id.imv1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[1]);
+            }
+        });
+        getActivity().findViewById(R.id.imv2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[2]);
+            }
+        });
+        getActivity().findViewById(R.id.imv3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[3]);
+            }
+        });
+        getActivity().findViewById(R.id.imv4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[4]);
+            }
+        });
+        getActivity().findViewById(R.id.imv5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[5]);
+            }
+        });
+        getActivity().findViewById(R.id.imv6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[6]);
+            }
+        });
+        getActivity().findViewById(R.id.imv7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[7]);
+            }
+        });
+        getActivity().findViewById(R.id.imv8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[8]);
+            }
+        });
+        getActivity().findViewById(R.id.imv9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append(number[9]);
+            }
+        });
+        getActivity().findViewById(R.id.imvAsterisk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String asterisk = "*";
+                editText.append(asterisk);
+            }
+        });
+        getActivity().findViewById(R.id.imvSharp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.append("#");
 
-            View.OnClickListener button1= new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    for(;;) {
-                        editText.setText("*");
-                    }
-                }
-            };
-            getActivity().findViewById(R.id.imv1).setOnClickListener(button1);
-        }
-
-
-
-
-
-
-
+            }
+        });
+    }
 
 }
 
