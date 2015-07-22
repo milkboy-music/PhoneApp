523e1aafb088b854caf471a28a9598d714582a05
milkboy-music <perspirationanddestiny@gmail.com>
2015-07-22 12:04:52 +0900

    test1

---
+++ app/src/main/java/jp/example/owner/jp/phoneappli/PreferenceActivity.java
@@ -0,0 +1,37 @@
+package jp.example.owner.jp.phoneappli;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PreferenceActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new CustomPreferenceFragment()).commit();   }
+
+
+
+    public static class CustomPreferenceFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+        }
+
+    }
+
+    }
+
+
+
